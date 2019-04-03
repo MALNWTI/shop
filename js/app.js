c755476b4b23fe60919e27dd5a4f79230d70a9c2
MALNWTI <MALNWTI1995@gmail.com>
2019-04-03 17:13:20 +0300

    All update

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  /*SLider*/
+  $(".slider_slick").slick({
+    infinite: false,
+    arrows: false,
+    speed: 1000,
+    dots:true,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+  /*Slider Close*/
+
+
+});
