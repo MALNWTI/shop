d8a9a4a7880f511f185f5522e96e3f27cc9499c3
MALNWTI <MALNWTI1995@gmail.com>
2019-04-05 15:50:04 +0300

    all fix

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 $(function(){
-
   /*SLider*/
   $(".previews_slick").slick({
     autoplay: true,
@@ -14,5 +13,27 @@ $(function(){
   });
   /*Slider Close*/
 
+  /*show button*/
+  $("#ShowMore").on("click",function(event) {
+    event.preventDefault();
+    $("#list_top").removeClass("max_h");
+  });
+  $("#ShowMor").on("click",function(event) {
+    event.preventDefault();
+    $("#product_list").removeClass("max_he");
+  });
+  /*show button*/
+
+  //Відсортування!!!
+  $('.categories_bottom button').click(function(){
+    var get_id = this.id;
+    var get_current = $('.product_list .' + get_id);
+    $('.list_item').not(get_current).hide(500);
+    get_current.show(500);
+  });
+  $('#All').click(function(){
+    $('.list_item').show(500);
+  });
+  //Відсортування кінець
 
 });
